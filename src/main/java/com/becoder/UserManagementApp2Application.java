8ff0bfe5c159e820e4346effdb442f1de3bdc599
Anuj <anujd4510@gmail.com>
2024-02-25 12:49:57 +0530

    Spring security authentication using webadaptersecurity method

---
+++ src/main/java/com/becoder/UserManagementApp2Application.java
@@ -0,0 +1,12 @@
+package com.becoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementApp2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApp2Application.class, args);
+		System.out.println("Anuj ❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️");
+	}
+}   
